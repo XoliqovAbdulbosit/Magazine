a04b8cb5bd2922a7efa07e02932c5d4d0c52f7ba
abdulbosit <abdulbositkhalikov96@gmail.com>
2024-09-14 16:45:09 +0500

    Final

--- Blog/settings.py
+++ Blog/settings.py
@@ -105,6 +105,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Media files
 # https://docs.djangoproject.com/en/5.0/howto/media-files/
